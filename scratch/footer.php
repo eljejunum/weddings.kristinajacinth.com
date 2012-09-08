735b207a5fb68b006ccddfffd878a104c73eae87
Jordan Mirrer <git@jordanmirrer.com>
2012-09-08 10:37:43 -0700

    current site - images optimized
    optimized all images to 1024 width and 60% jpeg compression.

---
+++ scratch/footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+
+	<footer>
+
+		<div id="share">
+        	<td>
+            
+            	<tr>
+                	<a href="http://pinterest.com/pin/create/button/?url=http%3A%2F%2Fweddings.kristinajacinth.com&media=http%3A%2F%2Fkristinajacinth.com%2Fweddings%2Fassets%2FfbShare.jpg&description=Let%20us%20capture%20your%20story...%20Boutique%20wedding%20and%20event%20photography%20in%20Southern%20California%2C%20New%20York%2C%20and%20destinations%20worldwide." class="pin-it-button" count-layout="horizontal"><img border="0" src="http://assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>
+        		<tr/>
+                <tr>
+                	<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fweddings.kristinajacinth.com&amp;send=false&amp;layout=buton_count&amp;width=500&amp;show_faces=false&amp;action=recommend&amp;colorscheme=light&amp;font=verdana&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:500px; height:35px;" allowTransparency="true"></iframe>
+                <tr/>
+            <td/>
+        
+        <div/>
+
+	<footer/>
+
+	<script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script>
+
+</body>
+</html>
